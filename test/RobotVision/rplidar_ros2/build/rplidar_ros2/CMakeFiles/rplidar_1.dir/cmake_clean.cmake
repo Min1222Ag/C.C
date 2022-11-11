abf9ff03ed0a5cf99070d9554332536aa5669f66
yhb1834 <pi@ubuntu.ksq.lcl>
2022-11-11 15:21:45 +0000

    rplidar package upload

---
+++ test/RobotVision/rplidar_ros2/build/rplidar_ros2/CMakeFiles/rplidar_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rplidar_1.dir/src/rplidar_1.cpp.o"
+  "rplidar_1"
+  "rplidar_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rplidar_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/RobotVision/rplidar_ros2/src/rplidar_ros2/src/rplidar_scan.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding:utf-8
+import numpy as np
+from time import sleep
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
