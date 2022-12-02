309a2441c92962a32ee893a5aab9a7cd00a49496
Min1222Ag <maexc834@naver.com>
2022-12-02 10:56:51 -0500

    socket communiation with ROS2 for path setting

--- test/PathSetting/path_ws/build/path_package/build/lib/path_package/publisher_member_function.py
+++ test/PathSetting/path_ws/build/path_package/build/lib/path_package/publisher_member_function.py
@@ -1,17 +1,4 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import time
 import socket
 import rclpy
 from rclpy.node import Node
@@ -26,39 +13,45 @@ class locationPublisher(Node):
 
     def __init__(self, socket, buf_size=4096, store_dir=STORE_DIR):
         super().__init__('location_publisher')
-        self.publisher_ = self.create_publisher(String, 'location', 10)
-        
+        self.publisher_ = self.create_publisher(String, 'location', 100)
+
         self.i = 0
         self.store_dir = store_dir
         self.buf_size = buf_size
 
+        socket.settimeout(3)
         self.socket = socket
         self.socket.listen()
-        conn, addr = self.socket.accept()
-        
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        
-        with conn:
-            print("Connected by {}".format(addr))
 
-        self.conn = conn
+        timer_period = 0.01  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
-        print(socket, self.i)
-        
+
         msg = String()
         msg.data = 'Hello World: %d' % self.i
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
-        data = self.conn.recv(self.buf_size)
-        b_num = self.conn.send(data)
-        print("{} bytes received.".format(b_num))
+        print("inner timer_callback")
 
-        with conn:
-            with open(self.store_dir+str(self.i)+".txt", "wb") as f:
-                f.write(data)
-            self.i += 1
+        try:
+            conn, addr = self.socket.accept()
+            self.conn = conn
+            data = self.conn.recv(self.buf_size)
+            b_num = len(data)
+            with self.conn:
+                with open(self.store_dir + str(self.i) + ".txt", "wb") as f:
+                    print("inner with open file")
+                    f.write(data)
+                    print("succeed in writing data!")
+
+            print("{}: {} bytes received.".format(self.i, b_num))
+        
+        except socket.timeout:
+            print("timeout {}".format(self.i))
+
+        self.i += 1
+
 
 def main(args=None):
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -72,7 +65,7 @@ def main(args=None):
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
+
     location_publisher.destroy_node()
     rclpy.shutdown()
 
