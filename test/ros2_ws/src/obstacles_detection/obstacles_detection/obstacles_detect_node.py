d274f74d7ef0048c4f3a57ac7780ad08cbdff742
Min1222Ag <74100211+Min1222Ag@users.noreply.github.com>
2022-12-06 16:53:23 -0500

    add comments

--- test/ros2_ws/src/obstacles_detection/obstacles_detection/obstacles_detect_node.py
+++ test/ros2_ws/src/obstacles_detection/obstacles_detection/obstacles_detect_node.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 # coding:utf-8
+
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 import rclpy
 from rclpy.node import Node
@@ -13,6 +28,7 @@ from std_msgs.msg import ByteMultiArray as yolo_arr  # for YOLO subscription mes
 import RPi.GPIO as GPIO
 import time
 
+# lidarDetect : using LiDAR information publish stop sign and subscribe image information from "yolo.py" 
 class lidarDetect(Node):
     def __init__(self):
         super().__init__('obstacles_detect_node'). # node name : obstacles_detect_node
@@ -93,7 +109,7 @@ class lidarDetect(Node):
         sleep(0.1)
      
     # pulisher(timer) callback function
-    # publish Stop, Left_Speed, Right_Speed 
+    # publish Stop, Left_Speed, Right_Speed through result from LiDAR (*Will be updated more*)
     def decision_callback(self):
         msg = Stop()
         msg.stop = self.Stop
